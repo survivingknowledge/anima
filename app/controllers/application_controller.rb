71e3e1d9beb7e7a105a79c02a14cd41984789139
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-08 13:43:10 -0400

    User controller now registers users and automatic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  before_action :current_user
+
   def authenticate!
     if !current_user || session[:user_id].empty?
       redirect_to new_session_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:show, :edit, :update, :destory]
+
   def new
   end
 
   def create
-    raise params.inspect
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
