67b7dc1168c21d5bef6b4c72721f9f0c2f421547
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-09 19:52:43 -0400

    Use session helper methods in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
+      log_in @user
       redirect_to root_path
     else
       render :new
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   #destroy a session
   def destroy
-    reset_session
+    log_out
     redirect_to root_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destory]
 
   def new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      log_in @user
       redirect_to root_path
     else
       render :new
