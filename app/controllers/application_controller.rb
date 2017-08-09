bd95d3f91be5c31af0387481099a39fea44ce192
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-09 19:46:44 -0400

    Session helper with login/logout and current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :current_user
-
   def authenticate!
-    if !current_user || session[:user_id].empty?
+    if current_user.nil?
       redirect_to new_session_path
     end
   end
 
   def current_user
-    @user ||= User.find(session[:user_id])
+    @user ||= User.find_by_id(session[:user_id])
   end
 end
