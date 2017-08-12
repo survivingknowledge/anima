6315d0118992daa471298b8b08ee73c1870e5b5e
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-12 07:55:10 -0400

    Removed turbolinks and action cable for now

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  it 'should get new' do
-    login_path
+  it 'sessions#new view is response 200 OK' do
+    #get maps to action: , autopicks controller of sessions/file
+    get :new
     expect(response.status).to eq(200)
   end
 end
