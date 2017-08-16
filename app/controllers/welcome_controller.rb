8d52210f0da01de53a35894f1354b971ecb6df13
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-15 23:21:04 -0400

    Welcome displays latest published chapters in english
    had to write out sql by hand as couldn't get arel to work properly with column alias will go back later and test if we can make it cleaner or shorter

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,20 @@
 class WelcomeController < ApplicationController
 
   def home
+    # @chapters = Novel.joins(:chapters).select('novels.title AS novel_title', 'chapters.*')
+    query = <<-SQL
+      SELECT c.title, c.chapter_number, c.publish_date, c.status, c.language, n.code AS novel_code, n.title AS novel_title
+      FROM chapters AS c
+      LEFT JOIN novels as n
+      ON c.novel_id = n.id
+      WHERE (c.publish_date <= "#{Date.today}")
+      AND (c.status = 'published')
+      AND (c.language = 'en')
+      ORDER BY c.publish_date DESC
+      LIMIT 10
+    SQL
+
+    @chapters = ActiveRecord::Base.connection.execute(query)
+
   end
 end
