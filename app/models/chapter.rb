68acad61d57311fb77b45458a3b52b3fe4eddeb2
Jordan Sleeper <survivingknowledge@gmail.com>
2017-08-29 21:02:35 -0400

    Chapter scope

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -4,17 +4,13 @@ class Chapter < ApplicationRecord
   #scopes (chainable class methods)
   default_scope { order(chapter_number: :desc)}
   scope :released, -> { where('publish_date <= ?', Date.today + 1)}
-  scope :public, -> { where(status: "published")}
+  scope :public_chapter, -> { where(status: "published")}
   scope :latest, -> { reorder(publish_date: :desc) }
   #languages
   scope :en, -> { where(language: "en") }
 
   def to_param
     chapter_number
-  end
-
-  def self.latest_chapters
-    self.where("publish_date <= ?", Date.today + 1).where(status: "published", language: "en").order(:publish_date => :desc).limit(10)
   end
 
 end
