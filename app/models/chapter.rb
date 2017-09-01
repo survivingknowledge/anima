3b32aad5cf2f6c92d6b711dbcfec95eca91cdfb3
Jordan Sleeper <survivingknowledge@gmail.com>
2017-09-01 10:21:58 -0400

    Used chapter scopes to clean up novel code

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,12 +2,13 @@ class Chapter < ApplicationRecord
   belongs_to :novel
 
   #scopes (chainable class methods)
-  default_scope { order(chapter_number: :desc)}
-  scope :released, -> { where('publish_date <= ?', Date.today + 1)}
-  scope :public_chapter, -> { where(status: "published")}
+  default_scope { order(chapter_number: :desc) }
+
+  scope :released, -> { where('publish_date <= ?', Date.today + 1).where(status: "published") }
   scope :latest, -> { reorder(publish_date: :desc) }
+  scope :find_ch, ->(num) { where('chapter_number = ?', num) if num.present? }
   #languages
-  scope :en, -> { where(language: "en") }
+  scope :lang, ->(lang = 'en') { where('language = ?', lang) }
 
   def to_param
     chapter_number
--- app/models/novel.rb
+++ app/models/novel.rb
@@ -1,20 +1,25 @@
 class Novel < ApplicationRecord
   has_many :chapters
 
+
+
   def to_param
     code
   end
 
   def find_chapter(num)
-    self.chapters.where(chapter_number: num, language: 'en').first
+    # self.chapters.where(chapter_number: num, language: 'en').first
+    self.chapters.find_ch(num).lang
   end
 
   def find_public_chapter(num)
-    self.chapters.where("publish_date <= ?", Date.today).where(chapter_number: num, status: 'published', language: 'en').first
+    # self.chapters.where("publish_date <= ?", Date.today).where(chapter_number: num, status: 'published', language: 'en').first
+    self.chapters.released.lang.find_ch(num).first
   end
 
   #should only have 1 published chapter for a given chapter number
   def find_public_chapters
-    self.chapters.where("publish_date <= ?", Date.today).where(status: 'published', language: 'en').order(chapter_number: :desc).limit(25)
+    # self.chapters.where("publish_date <= ?", Date.today).where(status: 'published', language: 'en').order(chapter_number: :desc).limit(25)
+    self.chapters.released.lang
   end
 end
